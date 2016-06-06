a401846e6c8a56e19ab12e0d818bd5d6fb90a7ea
thomyap <thomyap02@gmail.com>
2016-06-07 00:01:26 +0700

    tambah clsPembayaran

---
+++ Rumah Sakit/Class/clsPembayaran.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rumah_Sakit.Class
+{
+    class clsPembayaran
+    {
+        private int idPembayaran { set; get; }
+        private int idPasien { set; get; }
+        private int idRuang { set; get; }
+        private int idObatDibeli { set; get; }
+
+        public clsPembayaran(int idPasien, int idRuang, int idObatDibeli)
+        {
+            this.idPasien = idPasien;
+            this.idRuang = idRuang;
+            this.idObatDibeli = idObatDibeli;
+        }
+
+
+
+    }
+}
